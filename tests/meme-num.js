a4f1201c1eefd449eb1f7f3fec5a9449bfcf12b3
lucarlig <lucarlig@protonmail.com>
2021-08-11 09:36:47 +0100

    passing mem number trough

--- tests/meme-num.js
+++ tests/meme-num.js
@@ -1,5 +1,5 @@
 const anchor = require('@project-serum/anchor');
-
+const assert = require('assert');
 describe('meme-num', () => {
 
   // Configure the client to use the local cluster.
@@ -8,7 +8,21 @@ describe('meme-num', () => {
   it('Is initialized!', async () => {
     // Add your test here.
     const program = anchor.workspace.MemeNum;
-    const tx = await program.rpc.initialize();
-    console.log("Your transaction signature", tx);
+
+    const memeNumAccount = anchor.web3.Keypair.generate();
+
+    const tx = await program.rpc.initialize({
+      accounts: {
+        memeNumAccount: memeNumAccount.publicKey,
+        rent: anchor.web3.SYSVAR_RENT_PUBKEY
+      },
+      instructions: [
+        await program.account.memeNumAccount.createInstruction(memeNumAccount)],
+      signers: [memeNumAccount],
+    });
+
+    const memeNumAccountData = await program.account.memeNumAccount(memeNumAccount.publicKey);
+    assert(memeNumAccountData.memeNum.eq(new anchor.BN(420)));
+    // console.log("Your transaction signature", tx);
   });
 });
